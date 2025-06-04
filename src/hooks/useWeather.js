6792ca4fd82d0924a19c9975600376b6fcaae78b
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-06-04 12:16:32 +0530

    Update useWeather.js
    API env variable

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -3,9 +3,10 @@ import {useEffect,useState} from 'react'
 function useWeatherInfo(currentCity){
     const [weatherData, setWeatherData] = useState(null);
     const [error,setError] = useState(null);
+     const apiKey = import.meta.env.VITE_API_KEY;
     useEffect(()=>{
         if (!currentCity) return;
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=1d516562d451ece384ffed7db599f252&units=metric`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`)
         .then((respnse)=>respnse.json())
         .then((data)=>{
             if(data.cod===200){
